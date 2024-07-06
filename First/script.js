9a178ca59b8fc8a4c3d8919489eaf5b8af08b725
Abdalla-00 <abdalla.m.a.omar@gmail.com>
2024-07-06 11:23:16 -0400

    First

--- First/script.js
+++ First/script.js
@@ -1,26 +1,43 @@
 import * as THREE from "./three.module.js";
 
-// Scene
+//Scene
 const scene = new THREE.Scene();
+
+//Group
+const group = new THREE.Group();
 
 // Mesh
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: "purple" });
 const mesh = new THREE.Mesh(geometry, material);
+mesh.position.z = 1;
 
-scene.add(mesh);
+// Mesh Two
+const geometryT = new THREE.BoxGeometry(1, 1, 1)
+const materialT = new THREE.MeshBasicMaterial({ color: "green" });
+const meshT = new THREE.Mesh(geometryT, materialT);
+mesh.position.y = 2;
+
+// Adding both meshes inside group class
+group.add(mesh, meshT);
+group.position.x = 3;
+scene.add(group);
+
+// AxesHelper
+const axesHelper = new THREE.AxesHelper(4);
+scene.add(axesHelper);
 
 // Camera
 const aspect = {
     width: window.innerWidth,
     height: window.innerHeight
 }
-
+// near value is 1, and far value is 2000
 const camera = new THREE.PerspectiveCamera(75, aspect.width/aspect.height, 1, 2000);
-camera.position.z = 4;
 camera.position.x = 1;
 camera.position.y = 1;
-scene.add(camera)
+camera.position.z = 5;
+scene.add(camera);
 
 
 // Objects
